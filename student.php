d57626ae56ab30251224c12a6431cf9ce797c51f
RyanWestleyNZ <ryanwestleynz@gmail.com>
2016-09-15 10:35:36 +1200

    Add files via upload

---
+++ student.php
@@ -0,0 +1,17 @@
+<?php
+   include('session.php');
+?>
+<html">
+   
+   <head>
+		<link href="css/student.css" rel="stylesheet" type="text/css">
+		<title>Welcome </title>
+   </head>
+   
+   <body>
+      <h1>Welcome, <?php echo $login_session; ?></h1>
+	  <p>aaaaa</p>
+      <h2><a href = "logout.php">Sign Out</a></h2>
+   </body>
+   
+</html>
